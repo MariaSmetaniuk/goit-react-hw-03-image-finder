95be28793f7aff89ca7f1b5f53437a729b4a4d36
MariaSmetaniuk <smetaniukmaria@gmail.com>
2022-10-07 18:07:07 +0300

    change baseURL

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import PropTypes from 'prop-types';
 
-axios.defaults.baseURL = 'https://pixabay.com';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 const MY_KEY = '29177679-7b93f1d48a8f761360b3dae43';
 
 export const GetImages = async (query, page, per_page = 12) => {
   try {
-    const response = await axios.get('/api', {
+    const response = await axios.get('/', {
       params: {
         key: MY_KEY,
         q: query,
